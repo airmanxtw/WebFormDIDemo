371a0e192e1a29613c4be9a863a8b613fcec9ff1
airmanxtw <airmanx@stust.edu.tw>
2024-08-01 15:06:32 +0800

    新增 CSharpFunctionalExtensions 引用及修正 XML 格式
    在 Default.aspx.cs 中，新增了 CSharpFunctionalExtensions 的引用，並且新增了 Add 和 Doub 方法來取代原本的 App.Hello() 方法，這些方法使用了函數式編程的方式來處理數值運算和錯誤處理。
    
    在 Web.config 中，修正了多處 XML 格式，使其符合標準格式，主要是增加了 XML 標籤的結尾空格。
    
    在 WebFormDIDemo.csproj 中，新增了 CSharpFunctionalExtensions 的引用，使專案可以使用其提供的功能。
    
    在 packages.config 中，新增了 CSharpFunctionalExtensions 套件的引用，確保專案在還原 NuGet 套件時會包含該套件。

--- WebFormDIDemo/Default.aspx.cs
+++ WebFormDIDemo/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpFunctionalExtensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +14,17 @@ namespace WebFormDIDemo
         public  IAppService App { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            Label1.Text = App.Hello();
+            //Label1.Text = App.Hello();
+
+            Label1.Text = Add(-1, 2).Bind(Doub).Match(s => s.ToString(),f => f);
         }
+
+        public Result<int,string> Add(int a,int b)
+        {
+            return a>0 && b>0 ? Result.Success<int, string>(a + b) : Result.Failure<int, string>("Invalid input");
+        }
+
+        public Result<int,string> Doub(int v)=> Result.Success<int, string>(v * 2);
+
     }
 }
